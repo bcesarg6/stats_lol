2a91c617b744c44ec31f54da51ba87401279bd91
0Cristofer <cristoferoswald@gmail.com>
2015-04-27 16:37:57 -0300

    Changed inicial GUI

--- src/status_lol/inicialGUI.java
+++ src/status_lol/inicialGUI.java
@@ -24,7 +24,7 @@ import static javax.swing.WindowConstants.DISPOSE_ON_CLOSE;
  */
 public class inicialGUI extends JFrame{
     
-    JLabel lblTitle = new JLabel("Selecione");
+    JLabel lblTitle = new JLabel("Selecione sua opção");
     JButton btnVerStats = new JButton("Ver Stats");
     JButton btnInsJg = new JButton("Inserir novo jogo");
     
