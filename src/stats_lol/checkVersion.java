56d64a09aba305948d53e67099a5853f43a131f7
0Cristofer <cristoferoswald@gmail.com>
2015-04-28 20:00:44 -0300

    Comented the code

--- src/stats_lol/checkVersion.java
+++ src/stats_lol/checkVersion.java
@@ -15,27 +15,27 @@ import javax.swing.*;
  */
 public class checkVersion {
     
-    public String version = "a1.0";
-    public String currentVersion;
-    public String os = System.getProperty("os.name").toLowerCase();
-    public String[] ops = {"linux", "windows 7", "windows 8", "mac"};
-    public String versionURL = "http://pastebin.com/raw.php?i=tqZcCMdQ";
-    public String downloadURL = "http://pastebin.com/raw.php?i=tqZcCMdQ";
-    Runtime runtime = Runtime.getRuntime();
+    public String version = "a1.0"; //Version of the program
+    public String currentVersion; //Atual version
+    public String os = System.getProperty("os.name").toLowerCase(); //Gets user OS
+    public String[] ops = {"linux", "windows 7", "windows 8", "windows 8.1", "mac"}; //OSs
+    public String versionURL = "http://pastebin.com/raw.php?i=tqZcCMdQ"; //URL that is checked to see the version
+    public String downloadURL = "http://pastebin.com/raw.php?i=tqZcCMdQ"; //Download URL
+    Runtime runtime = Runtime.getRuntime(); //CMD
     
     public boolean checkVersion() throws URISyntaxException{
         try{
             URL checkPage = new URL(versionURL);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(new BufferedInputStream(checkPage.openConnection().getInputStream())));
-            currentVersion = reader.readLine().trim(); 
-            reader.close();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(new BufferedInputStream(checkPage.openConnection().getInputStream()))); //Reader
+            currentVersion = reader.readLine().trim(); //Puts the read data inside the currentVersion String
+            reader.close(); //Closes the reader
         } 
         catch (Exception E){};
-        if(currentVersion.equals(version)){
+        if(currentVersion.equals(version)){ //Verifys the program version and the actual version
             return true;
         }
         else{
-            final int updateQ = JOptionPane.showConfirmDialog(null,"LoL Stats Maker is outdated, update to the current version?","New Version avaible " + os,JOptionPane.YES_NO_OPTION);
+            final int updateQ = JOptionPane.showConfirmDialog(null,"LoL Stats Maker is outdated, update to the current version?","New Version avaible " + os,JOptionPane.YES_NO_OPTION); //Opens an window if the versions are diferent
             if(updateQ==0){
                 if(os.equals(ops[0])){
                     try{
@@ -50,7 +50,7 @@ public class checkVersion {
                         
                     }
                 }
-                else if(os.equals(ops[1]) || os.equals(ops[2])){
+                else if(os.equals(ops[1]) || os.equals(ops[2]) || os.equals(ops[3])){
                     try{
                         runtime.exec("rundll32 url.dll,FileProtocolHandler " + downloadURL);
                     }
