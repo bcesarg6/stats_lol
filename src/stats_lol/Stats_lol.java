531ca3d0d4bfb5d46ca7f8cf7344418a58c9c429
Bruno Cesar <bcesar.g6@gmail.com>
2017-10-24 16:36:28 -0200

    Atualizada lista de campeões

--- src/stats_lol/Stats_lol.java
+++ src/stats_lol/Stats_lol.java
@@ -16,13 +16,13 @@ import static stats_lol.Player.p;
 
 //This is the main class of the program. This will call the main GUI
 public class Stats_lol {
-    
+
     //Gets the user home
     //public static final String data = System.getProperty("user.home")+"/LoL Stats Maker/data/";
     public static final String data = "LoL Stats Maker/data/";
-    
+
     public static final String lang = Locale.getDefault().toLanguageTag();
-    
+
     public static final File dt = new File(data);
     public static final File config = new File(data+"data");
     public static final File players = new File(data+"players");
@@ -35,104 +35,104 @@ public class Stats_lol {
     public static final File tmpStage = new File(tmp.getAbsolutePath()+"/stage");
     public static final File rd = new File(rdm, "README.txt");
     public static final File[] dirs = {plDt, tmDt, chDt};
-    
+
     //Gets user OS
     public static String os = System.getProperty("os.name").toLowerCase();
-    
+
     //OSs
     public static String[] oss = {"linux", "windows 7", "windows 8", "windows 8.1", "mac"};
-    
+
     //Team names
     public static String team[] = t;
-    
+
     //Side names
     public static String sideB = "Blue";
     public static String sideR = "Red";
-    
+
     //Players names
     public static String player[] = p;
-    
+
     //Champions names
     public static final String champion[] = {"Aatrox", "Ahri", "Akali", "Alistar","Amumu",
-                                 "Anivia", "Annie", "Ashe", "Azir", "Bardo", "Blitzcrank",
+                                 "Anivia", "Annie", "Ashe", "Azir", "Bard", "Blitzcrank",
                                  "Brand", "Braum", "Caitlyn", "Cassiopeia","Cho'Gath",
                                  "Corki", "Darius", "Diana", "Draven", "Dr.Mundo", "Elise",
                                  "Evelynn", "Ezreal", "Fiddlesticks", "Fiora", "Fizz", "Galio",
                                  "Gangplank", "Garen", "Gnar", "Gragas", "Graves", "Hecarim",
-                                 "Heimerdinger", "Irelia", "Janna", "Jarvan IV", "Jax", "Jayce",
+                                 "Heimerdinger", "Irelia", "Janna", "Jarvan IV", "Jax", "Jayce", "Jhin",
                                  "Jinx", "Kalista", "Karma", "Karthus", "Kassadin", "Katarina",
-                                 "Kayle", "Kennen", "Kha'Zix","Kog'Maw", "LeBlanc", "Lee Sin",
+                                 "Kayle", "Kayn", "Kennen", "Kha'Zix","Kog'Maw", "LeBlanc", "Lee Sin",
                                  "Leona", "Lissandra", "Lucian", "Lulu", "Lux", "Malphite",
                                  "Malzahar",  "Maokai", "Master Yi", "Miss Fortune", "Mordekaiser",
                                  "Morgana", "Nami", "Nasus", "Nautilus","Nidalee", "Nocturne", "Nunu",
-                                 "Olaf", "Orianna", "Pantheon", "Poppy", "Quinn", "Rammus",  "Rek'Sai",
+                                 "Olaf", "Ornn", "Orianna", "Pantheon", "Poppy", "Quinn", "Rakan", "Rammus",  "Rek'Sai",
                                  "Renekton", "Rengar", "Riven",  "Rumble", "Ryze", "Sejuani", "Shaco",
-                                 "Shen", "Shyvana", "Singed", "Sion", "Sivir", "Skarner", "Sona", 
-                                 "Soraka", "Swain", "Syndra", "Talon", "Taric", "Teemo", "Thresh",
+                                 "Shen", "Shyvana", "Singed", "Sion", "Sivir", "Skarner", "Sona",
+                                 "Soraka", "Swain", "Syndra", "Taliyah", "Talon", "Taric", "Teemo", "Thresh",
                                  "Tristana", "Trundle", "Tryndamere", "Twisted Fate", "Tryndamere",
                                  "Udyr", "Urgot", "Varus", "Vayne", "Veigar", "Vel'Koz", "Vi",
-                                 "Viktor", "Vladimir", "Volibear", "Warwick", "Wukong", "Xerath",
+                                 "Viktor", "Vladimir", "Volibear", "Warwick", "Wukong", "Xayah", "Xerath",
                                  "Xin'Zhao", "Yasuo", "Yorick", "Zac", "Zed", "Ziggs", "Zilean", "Zyra"};
-    
+
     public static void main(String args[]) throws IOException, URISyntaxException{
         System.out.println(lang);
         new CheckVersion().checkVersion();
 
         if(!dt.exists()){
-            
-            dt.mkdir();        //Creates data directory             
+
+            dt.mkdir();        //Creates data directory
 
             if(!plDt.exists()){
                 plDt.mkdir();     //Creates plData directory
             }
-            
+
             if(!tmDt.exists()){
                 tmDt.mkdir();     //Creates tmData directory
             }
-            
+
             if(!chDt.exists()){
                 chDt.mkdir();    //Creates chData directory
             }
-            
+
             if(!rdm.exists()){
                 rdm.mkdir();    //Creates README directory
             }
-            
+
         }
 
-        if(!rd.exists()){           
+        if(!rd.exists()){
             rd.createNewFile(); //Creates README.txt file
         }
-        
+
         for(int i = 0; i < 40; i++){
-            
+
             File pl = new File(plDt, player[i]);
-            
+
             if(!pl.exists()){
                 pl.createNewFile(); //Creates README.txt file
-            }            
+            }
         }
-        
+
         for(int i = 0; i < 8; i++){
-            
+
             File tm = new File(tmDt, team[i]);
-            
+
             if(!tm.exists()){
                 tm.createNewFile(); //Creates README.txt file
-            }           
+            }
         }
-        
+
         for(int i = 0; i < 124; i++){
-            
+
             File ch = new File(chDt, champion[i]);
-            
+
             if(!ch.exists()){
                 ch.createNewFile(); //Creates README.txt file
-            }          
+            }
         }
-        
+
         try (FileWriter reW = new FileWriter(rd); BufferedWriter reB = new BufferedWriter(reW)) { //Set the writer to the REAMDE file
-            
+
             reB.write("Program made to create statistics for championships of the League of Legends game."); //Write this
             reB.newLine(); //Jumps to the next line
             reB.write("Chose between seeing actual championship stats or insert new game stats.");
@@ -146,6 +146,6 @@ public class Stats_lol {
         }
         ReadWrite rW = new ReadWrite();
         System.out.println(rW.readLines(rd.getAbsoluteFile())[1]);
-        InicialGUI inicialGUI = new InicialGUI(); //Calls InicialGUI        
+        InicialGUI inicialGUI = new InicialGUI(); //Calls InicialGUI
     }
 }
